0cfc159af58b6e788cbdd558eabcc964974cd89d
satwik singh <61342456+Satwik-Singh@users.noreply.github.com>
2021-06-20 00:47:40 +0530

    remove sen info

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDmjQAE67Rglw3l-zMMAKaMPsXySsCBSDA",
-    authDomain: "socialauth-3e1b6.firebaseapp.com",
-    projectId: "socialauth-3e1b6",
-    storageBucket: "socialauth-3e1b6.appspot.com",
-    messagingSenderId: "109418646701",
-    appId: "1:109418646701:web:59a5dba6943a1db2c98a24",
-    measurementId: "G-SQHTVGWW4J"
+    
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
